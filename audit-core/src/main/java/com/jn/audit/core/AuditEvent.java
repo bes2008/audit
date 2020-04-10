a46750de20110e4e8d7841fa12c14326a153e3c6
fs1194361820@163.com <fs1194361820@163.com>
2020-04-10 15:58:07 +0800

    * set the audit event

--- audit-core/src/main/java/com/jn/audit/core/AuditEvent.java
+++ audit-core/src/main/java/com/jn/audit/core/AuditEvent.java
@@ -5,4 +5,36 @@ import java.io.Serializable;
 public class AuditEvent implements Serializable {
     public static final long serialVersionUID = 1L;
     private String id;
+
+    // who
+    private String principalId;
+    private String principalName;
+    private PrincipalType principalType;
+    private String clientIp;    // {required} ip
+    private int clientPort;     // {required} port
+
+    // when
+    private long time;  // {required} UTC time
+
+    /**
+     * channel
+     */
+    private String service; // {required}
+    private String serviceProtocol; // HTTP, HTTPS, TCP
+    private String serviceIp;
+    private int servicePort;
+
+    /**
+     * target
+     */
+    private String resourceId;
+    private String resourceType;
+    private String resourceName;
+
+    /**
+     * do what
+     */
+    private String operateCode;
+    private String operateName;
+    private String parameters;
 }
---
+++ audit-core/src/main/java/com/jn/audit/core/PrincipalType.java
@@ -0,0 +1,7 @@
+package com.jn.audit.core;
+
+public enum PrincipalType {
+    authenticated,
+    anonymous,
+    unknown;
+}
