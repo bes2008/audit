d98163dae4735bf51c97928924b3ba9917af923b
fs1194361820@163.com <fs1194361820@163.com>
2020-04-10 14:39:13 +0800

    + add the event entity

---
+++ audit-core/src/main/java/com/jn/audit/core/AuditEvent.java
@@ -0,0 +1,8 @@
+package com.jn.audit.core;
+
+import java.io.Serializable;
+
+public class AuditEvent implements Serializable {
+    public static final long serialVersionUID = 1L;
+    private String id;
+}
