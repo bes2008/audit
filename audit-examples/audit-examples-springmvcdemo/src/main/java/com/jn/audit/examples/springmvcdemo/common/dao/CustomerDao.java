e76a3b87f4da3af00e3e4c45b07593a5d14f7366
fs1194361820@163.com <fs1194361820@163.com>
2020-04-13 16:49:51 +0800

    + initial spring mvc demo

---
+++ audit-examples/audit-examples-springmvcdemo/src/main/java/com/jn/audit/examples/springmvcdemo/common/dao/CustomerDao.java
@@ -0,0 +1,21 @@
+package com.jn.audit.examples.springmvcdemo.common.dao;
+
+import com.jn.audit.examples.springmvcdemo.common.model.Customer;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CustomerDao {
+    void insert(Customer user);
+
+    void updateById(Customer user);
+
+    void deleteById(String id);
+
+    List<Customer> selectByLimit(Customer limit);
+
+    Customer selectById(String id);
+
+    void batchInsert(List<Customer> users);
+}
