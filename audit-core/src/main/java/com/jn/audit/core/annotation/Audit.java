e856b83edfe77010e3b403d875f00df3314cca47
fs1194361820@163.com <fs1194361820@163.com>
2020-04-13 12:50:44 +0800

    * redefine the annotations

--- audit-core/src/main/java/com/jn/audit/core/annotation/Audit.java
+++ audit-core/src/main/java/com/jn/audit/core/annotation/Audit.java
@@ -1,5 +1,13 @@
 package com.jn.audit.core.annotation;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.*;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({TYPE, METHOD, PACKAGE})
 public @interface Audit {
 
 }
--- audit-core/src/main/java/com/jn/audit/core/annotation/Operation.java
+++ audit-core/src/main/java/com/jn/audit/core/annotation/Operation.java
@@ -1,5 +1,16 @@
 package com.jn.audit.core.annotation;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.METHOD;
+
+/**
+ * The operation definition annotation
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target({METHOD})
 public @interface Operation {
     String code() default "";    //  class fullname +"."+methodName
 
