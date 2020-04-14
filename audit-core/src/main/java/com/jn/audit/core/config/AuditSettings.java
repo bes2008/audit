975abc6f9ae6cc7f8c0f3bc0f0430b569db2eddd
fs1194361820@163.com <fs1194361820@163.com>
2020-04-14 18:43:56 +0800

    + add audit setting

---
+++ audit-core/src/main/java/com/jn/audit/core/config/AuditSettings.java
@@ -0,0 +1,13 @@
+package com.jn.audit.core.config;
+
+public class AuditSettings {
+    private String operationDefinitionResource;
+
+    public String getOperationDefinitionResource() {
+        return operationDefinitionResource;
+    }
+
+    public void setOperationDefinitionResource(String operationDefinitionResource) {
+        this.operationDefinitionResource = operationDefinitionResource;
+    }
+}
