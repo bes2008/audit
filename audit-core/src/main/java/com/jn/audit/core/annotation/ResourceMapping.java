97ee2f14f840be78bd37481c61aa496a6b597b70
fs1194361820@163.com <fs1194361820@163.com>
2020-05-13 17:12:17 +0800

    * 重构代码

--- audit-core/src/main/java/com/jn/audit/core/annotation/ResourceMapping.java
+++ audit-core/src/main/java/com/jn/audit/core/annotation/ResourceMapping.java
@@ -4,13 +4,14 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import static java.lang.annotation.ElementType.PARAMETER;
 import static java.lang.annotation.ElementType.TYPE;
 
 /**
  * 用在一个Entity类上，用于标注 分别代表 id, name的字段名
  */
 @Retention(RetentionPolicy.RUNTIME)
-@Target({TYPE})
+@Target({TYPE, PARAMETER})
 public @interface ResourceMapping {
     /**
      * @see com.jn.audit.core.model.Resource resourceName
