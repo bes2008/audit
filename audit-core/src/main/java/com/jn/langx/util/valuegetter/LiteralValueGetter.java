0bbadfa7a1e3d8a05dd0d4c008a341a97e206b5b
fs1194361820@163.com <fs1194361820@163.com>
2020-05-14 19:08:39 +0800

    * 升级 langx-java 到 2.3.0版本

--- audit-core/src/main/java/com/jn/langx/util/valuegetter/LiteralValueGetter.java
+++
@@ -1,8 +0,0 @@
-package com.jn.langx.util.valuegetter;
-
-public class LiteralValueGetter<V> implements ValueGetter<V, V> {
-    @Override
-    public V get(V input) {
-        return input;
-    }
-}
--- audit-core/src/main/java/com/jn/langx/util/valuegetter/ValueGetter.java
+++
@@ -1,7 +0,0 @@
-package com.jn.langx.util.valuegetter;
-
-import com.jn.langx.factory.Factory;
-
-public interface ValueGetter<T,V> extends Factory<T,V> {
-
-}
