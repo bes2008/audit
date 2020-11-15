370909c8e2df66d07acae81d8744e81999c269d5
fjn <1194361820@qq.com>
2020-11-15 21:20:26 +0800

    * 为基于ESMVC框架的应用提供ElasticSearch 上资源的访问

---
+++ audit-entityloader/audit-entityloader-esmvc/src/main/java/com/jn/audit/entityloader/esmvc/EsmvcEntityLoader.java
@@ -0,0 +1,79 @@
+package com.jn.audit.entityloader.esmvc;
+
+import com.jn.audit.core.model.ResourceDefinition;
+import com.jn.audit.core.resource.idresource.EntityLoader;
+import com.jn.esmvc.service.ESModelService;
+import com.jn.langx.factory.Factory;
+import com.jn.langx.util.ClassLoaders;
+import com.jn.langx.util.Emptys;
+import com.jn.langx.util.Preconditions;
+import com.jn.langx.util.Strings;
+import com.jn.langx.util.collection.MapAccessor;
+import com.jn.langx.util.collection.Pipeline;
+import com.jn.langx.util.function.Function;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class EsmvcEntityLoader implements EntityLoader<Object> {
+    private static final String SERVICE_IMPL_CLASS_NAME = "serviceImplClassName";
+    private static final Logger logger = LoggerFactory.getLogger(EsmvcEntityLoader.class);
+
+    private String name = "esmvc";
+    private Factory<Class, ESModelService> esModelServiceFactory;
+
+    public void setName(String name) {
+        if (Emptys.isNotEmpty(name)) {
+            this.name = name;
+        }
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public List<Object> load(ResourceDefinition resourceDefinition, List<Serializable> ids) {
+        Preconditions.checkNotNull(esModelServiceFactory, "the EsModelServiceFactory is null or empty");
+        if (Emptys.isEmpty(ids)) {
+            return null;
+        }
+        MapAccessor mapAccessor = resourceDefinition.getDefinitionAccessor();
+        String serviceImplClassName = mapAccessor.getString(SERVICE_IMPL_CLASS_NAME);
+        Preconditions.checkNotEmpty(serviceImplClassName, "the {} is undefined in the resource definition", SERVICE_IMPL_CLASS_NAME);
+        Class serviceImplClass = null;
+        try {
+            serviceImplClass = ClassLoaders.loadImplClass(serviceImplClassName, EsmvcEntityLoader.class.getClassLoader(), ESModelService.class);
+        } catch (Throwable ex) {
+            logger.error("Can't find the ESModel : {}", serviceImplClassName);
+            return null;
+        }
+        ESModelService esModelService = esModelServiceFactory.get(serviceImplClass);
+        List<String> stringIds = Pipeline.of(ids).map(new Function<Serializable, String>() {
+            @Override
+            public String apply(Serializable id) {
+                return id.toString();
+            }
+        }).asList();
+
+        List entities = null;
+        try {
+            entities = esModelService.getByIds(stringIds);
+        } catch (Throwable ex) {
+            logger.error("Error occur when loading {} instances, ids: {}", serviceImplClassName, Strings.join(", ", stringIds));
+        }
+        return entities;
+
+    }
+
+    public Factory<Class, ESModelService> getEsModelServiceFactory() {
+        return esModelServiceFactory;
+    }
+
+    public void setEsModelServiceFactory(Factory<Class, ESModelService> esModelServiceFactory) {
+        this.esModelServiceFactory = esModelServiceFactory;
+    }
+}
