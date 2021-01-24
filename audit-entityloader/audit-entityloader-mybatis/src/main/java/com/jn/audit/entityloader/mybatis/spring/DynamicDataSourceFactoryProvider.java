228d30d2b641397487267f0fd73876af7fb138b8
fjn <1194361820@qq.com>
2021-01-24 15:17:35 +0800

    * 准备2.3.0迭代

--- audit-entityloader/audit-entityloader-mybatis/src/main/java/com/jn/audit/entityloader/mybatis/spring/DynamicDataSourceFactoryProvider.java
+++ audit-entityloader/audit-entityloader-mybatis/src/main/java/com/jn/audit/entityloader/mybatis/spring/DynamicDataSourceFactoryProvider.java
@@ -1,13 +1,15 @@
 package com.jn.audit.entityloader.mybatis.spring;
 
 import com.jn.audit.core.AuditRequest;
+import com.jn.langx.annotation.NonNull;
+import com.jn.langx.annotation.Nullable;
 import com.jn.sqlhelper.datasource.key.DataSourceKeySelector;
 import com.jn.sqlhelper.datasource.key.MethodInvocationDataSourceKeySelector;
 import com.jn.sqlhelper.mybatis.spring.session.factory.dynamicdatasource.DynamicDataSourceSqlSessionFactoryProvider;
 import com.jn.sqlhelper.mybatis.spring.session.factory.dynamicdatasource.DynamicSqlSessionFactory;
 
 public class DynamicDataSourceFactoryProvider extends DynamicDataSourceSqlSessionFactoryProvider<AuditRequest> {
-    public DynamicDataSourceFactoryProvider(MethodInvocationDataSourceKeySelector delegateSelector, DynamicSqlSessionFactory dynamicSqlSessionFactory) {
+    public DynamicDataSourceFactoryProvider(@Nullable MethodInvocationDataSourceKeySelector delegateSelector, @NonNull DynamicSqlSessionFactory dynamicSqlSessionFactory) {
         this(new AuditRequestDataSourceKeySelector(delegateSelector), dynamicSqlSessionFactory);
     }
 
