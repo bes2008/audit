7dfbc4622098f7067525b4f8ee53339927520a8b
e <262506529@qq.com>
2021-05-17 14:12:15 +0800

    增加CommonsProps类，并使model相关类继承，便于自定义扩展。

--- audit-core/src/main/java/com/jn/audit/core/model/CommonProps.java
+++ audit-core/src/main/java/com/jn/audit/core/model/CommonProps.java
@@ -3,13 +3,16 @@ package com.jn.audit.core.model;
 import com.jn.langx.util.collection.Collects;
 import com.jn.langx.util.collection.MapAccessor;
 
+import java.io.Serializable;
 import java.util.Map;
 
 /**
  * Extended properties
  * 便于个性化扩展的属性
  */
-public class CommonProps {
+public class CommonProps implements Serializable {
+    public static final long serialVersionUID = 1L;
+
     protected Map<String, Object> props;
 
     public Map<String, Object> getProps() {
